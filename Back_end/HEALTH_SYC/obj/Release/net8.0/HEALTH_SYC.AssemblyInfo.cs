716aa72bfc3b1ee55daadca71de92760826d0884
mohamedhoussien <mohamed24houssien@gmail.com>
2025-01-20 23:27:10 +0200

    first commit

--- Back_end/HEALTH_SYC/obj/Release/net8.0/HEALTH_SYC.AssemblyInfo.cs
+++ Back_end/HEALTH_SYC/obj/Release/net8.0/HEALTH_SYC.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("HEALTH_SYC")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3ab124aec5e213cc73fa21a052527af6efb65020")]
 [assembly: System.Reflection.AssemblyProductAttribute("HEALTH_SYC")]
